729e032afb88a2527fc1ffbcd87974f5f4023c36
W014ara <bezruk2000@mail.ru>
2021-06-05 23:22:46 +0300

    fixed gradient bar

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,1 +1,22 @@
-console.log('Console works!')
+
+window.addEventListener("resize", (e)=>{
+    setGradientWidth();
+})
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    setGradientWidth();
+})
+
+
+function setGradientWidth(){
+    if(Number(screen.width) === 2560){
+        return 0;
+    }
+    const gradientBar = document.querySelector(".gradient");
+    const targetDiv = document.querySelector(".main-general__card-block");
+    let targetpx = (Number(screen.width) - Number(targetDiv.clientWidth))/2;
+    let targetpercent = (targetpx * 100) / screen.width;
+    console.log(targetpx, Math.floor(targetpercent));
+    gradientBar.style.right = `${targetpercent}%`;
+    return 1;
+}
