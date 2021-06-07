1db4b4e0fd25d0420dd1433d9bafafdb11610167
W014ara <bezruk2000@mail.ru>
2021-06-07 23:47:44 +0300

    added link scrolling logic

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,7 +1,7 @@
 const articles = document.querySelector(".articles__items");
 const advertisement = document.querySelector(".articles__advertisement");
+const advertisementItem = document.querySelector(".articles__advertisement-item");
 const icon = document.querySelector('.header-general__logo-icon');
-
 
 window.onload = function(){
     if(Number(screen.width) <= 768){
@@ -30,3 +30,14 @@ let observer = new IntersectionObserver(function (entries) {
 });
 
 observer.observe(icon);
+
+window.addEventListener("scroll", (e)=>{
+    let currentOffset = window.scrollY;
+    let fullpageSize = document.body.scrollHeight;
+    let result = currentOffset * 100 / fullpageSize;
+    if(result >= 20){
+        advertisementItem.classList.add("_active");
+    }else{
+        advertisementItem.classList.remove("_active");
+    }
+})
